cd1723878779ff209a475b806f1dbef0bd2f840a
Pei Ern Koh <peiern.28@gmail.com>
2021-09-10 14:34:36 +0800

    added .cards

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -8,10 +8,21 @@ class ListsController < ApplicationController
   end
 
   def new
-
+    @list = List.new
   end
 
   def create
+    @list = List.new(list_params)
+    if @list.save
+      redirect_to lists_path
+    else
+      render :new
+    end
+  end
 
+  private
+
+  def list_params
+    params.require(:list).permit(:name)
   end
 end
